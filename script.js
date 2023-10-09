8db9db4a0aa533c7086c1e3c365a300cb0944ebd
Stefania <stefania.struzzi@gmail.com>
2023-10-09 21:32:41 +0200

    creata funzione get results

---
+++ script.js
@@ -0,0 +1,67 @@
+// Dove mostrare risultati 
+let resultsContainer = document.querySelector('#results-container')
+
+// Tutti i risultati
+let allResults = []
+
+
+window.onload = async function () {
+
+    try {
+        allResults = await getResults()
+        console.log(allResults)
+
+        // Varibili per identificare nodi
+        resultsContainer = document.querySelector('#results-container')
+
+        // Mostro tutti i prodotti nel DOM
+        displayResults(allResults)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Funzione che richiama risultati
+
+async function getResults() {
+
+    try {
+        const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=pianoguys')
+
+        const jsonData = await response.json()
+        return jsonData
+        
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+
+// Funzione che mostra risultati
+
+function displayResults(result) {
+
+
+    const songs = result.data
+    console.log(songs)
+
+    resultsContainer.innerHTML = songs.map( result => /*html*/`
+    <div class="col">
+    <div class="card">
+        <img src="${result.artist}" class="card-img-top" alt="${result.album}">
+        <div class="card-body">
+          <p class="card-text">${result.duration}</p>
+        </div>
+      </div>
+</div>
+                                `
+    ).join('')
+    
+}
+
+
+
+
+
