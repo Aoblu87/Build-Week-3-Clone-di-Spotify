67faae4b501d59c0623c86685848115d7c84ef7d
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-10-10 21:00:00 +0200

    js aggiornato

--- artist/artist.js
+++ artist/artist.js
@@ -43,19 +43,13 @@ async function loadData(id) {
     
     
           cont.innerHTML = /*html*/` 
-                       <div class="container d-flex">
-                         <img class="image col-12 col-lg-3" src="${data.imageUrl}" alt="">
-                         <div class="d-flex flex-column ms-5 info">
-                           <h1 class="name">${data.name}</h1>
-                           <h4 class="description">${data.description}</h4>
-                           <h4 class="brand">${data.brand}</h4>
-                           <h3 class="price">Price: ${data.price} €</h3>
-                           <div class="d-flex">
-                             <a href="../index.html" class=""> <button class="btn btn-outline-dark"><i class="bi bi-arrow-left-short"></i>Back</button></a>
-                           </div>
-                         </div>
-                         
-                       </div>`;
+                        <div class="sfondo-img">
+
+                        </div>
+  
+                        <div>
+  
+                        </div>`;
     }
 
     window.onload = async function () {
