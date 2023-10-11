560f9bf98ebad4ed7cd79b9fca6357cdecfb8b5e
Alessio <belcastroalessio@outlook.it>
2023-10-11 12:28:11 +0200

    Added Bottom Play Bat

--- search/search.js
+++ search/search.js
@@ -1,4 +1,15 @@
 const SearchInput = document.querySelector('#SearchBar')
+const player = document.querySelector('#player')
+
+function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0'); // Aggiunge uno zero iniziale se necessario
+    const formattedSeconds = String(seconds).padStart(2, '0'); // Aggiunge uno zero iniziale se necessario
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getData(query) {
     const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=' + query)
@@ -13,23 +24,26 @@ async function displayResults(results) {
 
     if (results.length <= 3) {
         row.innerHTML = ''
+
     } else {
 
         const data = await getData(results)
 
+        console.log(data);
+
         row.innerHTML = data.map(({ album, artist, title, duration, preview, id }) => /*html*/`
     
         <div class="row mb-4" id="_${id}" onclick="playAudio(${id})">
-            <div class="col-2 col-md-1 pe-0">
+            <div class="col-2 col-md-1">
                 <div style="position: relative">
-                    <img src="${album.cover_big}" alt="" class="img-fluid">
+                    <img src="${album.cover_big}" alt="" class="img-fluid rounded">
                     <audio src="${preview}"></audio>
                     <div class="play-button d-none d-md-flex"><i class="bi bi-play-fill text-white"></i></div>
                 </div>
             </div>
             <div class="col d-flex flex-column justify-content-center TextCut">
                 <h5 class="mb-1 fs-6 fw-semibold TextCut">${title}</h5>
-                <p class="text-white-50 mb-0 fw-semibold TextCut">${artist.name}</p>
+                <p class="mb-0 text-white-50 fw-semibold TextCut">${artist.name}</p>
             </div>
             <div class="col-auto d-flex align-items-center">
                 <p class="text-white-50 fw-bold">${formatTime(duration)}</p>
@@ -40,42 +54,64 @@ async function displayResults(results) {
     }
 }
 
-function formatTime(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time % 60);
+async function playAudio(id) {
+    const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/track/" + id)
+    const data = await response.json()
 
-    const formattedMinutes = String(minutes).padStart(2, '0'); // Aggiunge uno zero iniziale se necessario
-    const formattedSeconds = String(seconds).padStart(2, '0'); // Aggiunge uno zero iniziale se necessario
+    const { title, album, artist } = data
 
-    return `${formattedMinutes}:${formattedSeconds}`;
-}
-
-function playAudio(id) {
     const target = document.querySelector(`#_${id}`)
-    const title = target.querySelector('h5')
+
+    const songName = target.querySelector('h5')
     const CurrentAudio = target.querySelector(`audio`)
     const icon = target.querySelector(".play-button")
 
 
-    if (CurrentAudio.paused) {
+    if (CurrentAudio.paused) { /* SE L'AUDIO E' IN PAUSA O NON PARTITO*/
 
         CurrentAudio.play();
+
+        player.style.transform = "translateY(0px)"
+        player.innerHTML = `
+        <div class="row mb-4 mx-0 mx-md-5 p-2 bg-dark rounded d-flex align-items-center">
+            <div class="col-auto px-0">
+                <div style="position: relative">
+                    <img src="${album.cover_big}" alt="" class="img-fluid rounded shadow" style="width: 4rem">
+                </div>
+            </div>
+            <div class="col d-flex flex-column justify-content-center TextCut">
+                <h5 class="mb-1 fs-6 fw-semibold TextCut">${title}</h5>
+                <p class="mb-0 text-white-50 TextCut">${artist.name}</p>
+            </div>
+            <div class="col-auto d-flex align-items-center">
+                <i class="bi bi-heart mx-3"></i>
+                <i class="bi bi-play-fill"></i>
+
+            </div>
+        </div>
+        `
+
         icon.innerHTML = '<i class="bi bi-pause-fill text-white"></i>'
         icon.classList.toggle("active")
-        title.style.color = "var(--color-green)"
+        songName.style.color = "var(--color-green)"
 
         CurrentAudio.addEventListener('ended', function () {
+            player.style.transform = "translateY(150px)"
+
             icon.innerHTML = '<i class="bi bi-play-fill text-white"></i>';
             icon.classList.remove("active")
-            title.style.color = "#fff"
+            songName.style.color = "#fff"
         });
 
-    } else {
+    } else { /* SE L'AUDIO E' IN RIPRODUZIONE */
         CurrentAudio.pause();
+
+        player.style.transform = "translateY(150px)"
+
         icon.innerHTML = '<i class="bi bi-play-fill text-white"></i>'
         icon.classList.toggle("active")
-        title.style.color = "#fff"
+        songName.style.color = "#fff"
     }
 
-    
+
 }
